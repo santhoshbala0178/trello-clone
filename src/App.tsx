b428cf356657e2f510e2a23073b48979b1e614e3
Sandy <santhoshbala0718@gmail.com>
2021-10-06 15:56:02 +0530

    first commti

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
-          
+
           <code>src/App.tsx</code>
           and save to reload.
         </p>
@@ -25,6 +25,7 @@ function App() {
     </div>
   );
 
+  
   
 }
 
