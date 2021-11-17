4715ce780ee9d5e015e41a886c8d25fd59846c87
Sandy <santhoshbala0718@gmail.com>
2021-11-17 17:06:10 +0530

    Committing changes

--- src/components/BoardPage/BoardStar/BoardStar.tsx
+++ src/components/BoardPage/BoardStar/BoardStar.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import BoardStarContainer from "./BoardStar.style";
-import IconHolder from "../../Common/IconHolder/IconHolder";
+import React from 'react'
+import BoardStarContainer from './BoardStar.style'
+import IconHolder from '../../Common/IconHolder/IconHolder'
 
 const BoardStar = () => (
-  <BoardStarContainer>
-    <IconHolder path="assets/StarIcon.svg" width={16} color="#FFF" />
-  </BoardStarContainer>
-);
+    <BoardStarContainer>
+        <IconHolder path="assets/StarIcon.svg" width={16} color="#FFF" />
+    </BoardStarContainer>
+)
 
-export default BoardStar;
+export default BoardStar
--- src/components/Popup/CreateWorkspacePopup/CreateWorkspacePopup.tsx
+++ src/components/Popup/CreateWorkspacePopup/CreateWorkspacePopup.tsx
@@ -1,76 +1,81 @@
-import React, { useState } from "react";
-import { connect, ConnectedProps, useDispatch } from "react-redux";
-import { CREATE_WORKSPACE_POPUP } from "../../../constants/actionTypes";
+import React, { useState } from 'react'
+import { connect, ConnectedProps, useDispatch } from 'react-redux'
+import { CREATE_WORKSPACE_POPUP } from '../../../constants/actionTypes'
 import {
-  PopupHolder,
-  CreateWorkSpace,
-  Header,
-  Description,
-  WorkSpaceName,
-  TextHolder,
-  WorkspaceNameEditor,
-  WorkspaceNameDescription,
-  CreateButton,
-  CreateWorkspaceContainer,
-} from "./CreateWorkspacePopup.style";
-import { IconContainer } from "../CreatePopup/CreatePopup.style";
-import IconHolder from "../../Common/IconHolder/IconHolder";
-import popupStateAction from "../../../actions/popupStateAction";
+    PopupHolder,
+    CreateWorkSpace,
+    Header,
+    Description,
+    WorkSpaceName,
+    TextHolder,
+    WorkspaceNameEditor,
+    WorkspaceNameDescription,
+    CreateButton,
+    CreateWorkspaceContainer,
+} from './CreateWorkspacePopup.style'
+import { IconContainer } from '../CreatePopup/CreatePopup.style'
+import IconHolder from '../../Common/IconHolder/IconHolder'
+import popupStateAction from '../../../actions/popupStateAction'
 
 const mapDispatchToProps = {
-  popupStateActionProp: popupStateAction,
-};
+    popupStateActionProp: popupStateAction,
+}
 
-const connector = connect(null, mapDispatchToProps);
+const connector = connect(null, mapDispatchToProps)
 
-type Props = ConnectedProps<typeof connector>;
+type Props = ConnectedProps<typeof connector>
 
 const CreateWorkSpacePopup: React.FC<Props> = ({ popupStateActionProp }) => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
-  const closePopup = () => {
-    dispatch(popupStateActionProp(CREATE_WORKSPACE_POPUP, false));
-  };
+    const closePopup = () => {
+        dispatch(popupStateActionProp(CREATE_WORKSPACE_POPUP, false))
+    }
 
-  const headerText = "Let's build a Workspace";
-  const descriptionText =
-    "Boost your productivity by making it easier for everyone to access boards in one location.";
-  const [workSpaceName, setWorkspaceName] = useState("");
+    const headerText = "Let's build a Workspace"
+    const descriptionText =
+        'Boost your productivity by making it easier for everyone to access boards in one location.'
+    const [workSpaceName, setWorkspaceName] = useState('')
 
-  const onNameEdit = (e: React.FormEvent<HTMLInputElement>) => {
-    setWorkspaceName(e?.currentTarget?.value);
-  };
+    const onNameEdit = (e: React.FormEvent<HTMLInputElement>) => {
+        setWorkspaceName(e?.currentTarget?.value)
+    }
 
-  return (
-    <PopupHolder onClick={closePopup}>
-      <CreateWorkSpace onClick={(e) => e.stopPropagation()}>
-        <Header>
-          {headerText}
-          <IconContainer onClick={closePopup}>
-            <IconHolder path="assets/CloseIcon.svg" width={22} color="#000" />
-          </IconContainer>
-        </Header>
-        <Description>{descriptionText}</Description>
-        <CreateWorkspaceContainer>
-          <WorkSpaceName>
-            <TextHolder>Workspace name</TextHolder>
-            <WorkspaceNameEditor
-              placeholder="Taco's Co."
-              value={workSpaceName}
-              onChange={onNameEdit}
-            />
-            <WorkspaceNameDescription>
-              This is the name of your company, team or organization.
-            </WorkspaceNameDescription>
-          </WorkSpaceName>
+    return (
+        <PopupHolder onClick={closePopup}>
+            <CreateWorkSpace onClick={(e) => e.stopPropagation()}>
+                <Header>
+                    {headerText}
+                    <IconContainer onClick={closePopup}>
+                        <IconHolder
+                            path="assets/CloseIcon.svg"
+                            width={22}
+                            color="#000"
+                        />
+                    </IconContainer>
+                </Header>
+                <Description>{descriptionText}</Description>
+                <CreateWorkspaceContainer>
+                    <WorkSpaceName>
+                        <TextHolder>Workspace name</TextHolder>
+                        <WorkspaceNameEditor
+                            placeholder="Taco's Co."
+                            value={workSpaceName}
+                            onChange={onNameEdit}
+                        />
+                        <WorkspaceNameDescription>
+                            This is the name of your company, team or
+                            organization.
+                        </WorkspaceNameDescription>
+                    </WorkSpaceName>
 
-          <CreateButton disabled={workSpaceName.length === 0}>
-            Create
-          </CreateButton>
-        </CreateWorkspaceContainer>
-      </CreateWorkSpace>
-    </PopupHolder>
-  );
-};
+                    <CreateButton disabled={workSpaceName.length === 0}>
+                        Create
+                    </CreateButton>
+                </CreateWorkspaceContainer>
+            </CreateWorkSpace>
+        </PopupHolder>
+    )
+}
 
-export default connector(CreateWorkSpacePopup);
+export default connector(CreateWorkSpacePopup)
